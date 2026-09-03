1031ea39145ce3ce5ced29f0fe90db2dacaad499
EderYaycate <eder.yaycate@tecsup.edu.pe>
2026-09-02 19:19:03 -0500

    Agrega README con capturas y respuesta sobre remember

--- Semana03/LAB03SinIA/app/src/main/java/com/yaycate/lab03sinia/MainActivity.kt
+++ Semana03/LAB03SinIA/app/src/main/java/com/yaycate/lab03sinia/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -54,6 +56,7 @@ fun PantallaRegistro(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         Text(
             text = "Nuevo producto",
